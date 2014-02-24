99fd7884d419c0917a995a5dcfaf680e5c6227a5
Zeke Zhao | 赵望野 <wangye.zhao@wandoujia.com>
2014-02-24 16:40:58 +0800

    Add basic pages.

---
+++ app/javascripts/fe101.js
@@ -0,0 +1,71 @@
+/* 
+* @Author: hanjiyun
+* @Date:   2014-02-22 11:47:22
+* @Last Modified by:   hanjiyun
+* @Last Modified time: 2014-02-24 11:58:10
+*/
+
+$(function(){
+    // if($(window).width() > 1220 ){
+    //     $('.ui.sidebar').sidebar('show');
+
+    //     $('article').click(function(){
+    //         $('.ui.sidebar').sidebar('toggle');
+    //     })
+    // } else {
+    //      $('#switch_btn').click(function(){
+    //         $('.ui.sidebar').sidebar('toggle');
+    //     })
+    // }
+        $('#switch_btn').click(function(){
+            // $('.ui.sidebar').sidebar('toggle');
+            $('body').toggleClass('pushed')
+        })
+
+    $('.sidebar a').click(function(){
+        var href = $(this).attr('href');
+        // console.log(href);
+
+        $('article section').removeClass('active');
+
+        console.log($(href+''))
+        $(href+'').addClass('active')
+    })
+
+    $('.item:not(.active)').on('click', 'a', function(){
+        var $t = $(this);
+        var tab = $t.attr('href').replace('#', '');
+
+        console.log(tab)
+
+        $('.item').removeClass('active');
+        $t.parents('.item').eq(0).addClass('active');
+
+        $('section').removeClass('active')
+        $('#' + tab).addClass('active')
+
+        setTimeout(function(){
+            if($(window).width() < 769 ){
+                $('body').removeClass('pushed');
+            }
+        }, 300)
+
+
+        // $('section').animate({scrollTop:0},0);
+    })
+
+    // $(window).resize(function(){
+    //     var width = $(window).width();
+    //     if(width < 1220){
+    //         $('.ui.sidebar').sidebar('hide');
+    //         $('#switch_btn').show();
+    //         // $('#switch_btn').click(function(){
+    //         //     $('.ui.sidebar').sidebar('toggle');
+    //         // })
+    //     } else {
+    //         $('.ui.sidebar').sidebar('show');
+    //         $('#switch_btn').hide();
+    //     }
+    // })
+})
+
